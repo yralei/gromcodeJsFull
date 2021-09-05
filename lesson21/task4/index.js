f7be2212c5786d7fb388a27c0b11df429173a87b
Iurii <yralei@gmail.com>
2021-09-05 12:53:40 +0300

    lesson21task4

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,11 +1,11 @@
-const getTitle = () => document.querySelector('.title').textContent;
+export const getTitle = () => document.querySelector('.title').textContent;
 console.log(getTitle());
 
-const getDescription = () => document.querySelector('.about').innerText;
+export const getDescription = () => document.querySelector('.about').innerText;
 console.log(getDescription());
 
-const getPlans = () => document.querySelector('.plans').innerHTML;
+export const getPlans = () => document.querySelector('.plans').innerHTML;
 console.log(getPlans());
 
-const getGoal = () => document.querySelector('.goal').outerHTML;
+export const getGoal = () => document.querySelector('.goal').outerHTML;
 console.log(getGoal());
