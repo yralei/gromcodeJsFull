2ee51607e60c35e5c5184a0cdd2395fec7d72fa6
Iurii <yralei@gmail.com>
2021-09-05 13:46:06 +0300

    lesson21task6

---
+++ lesson21/task6/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-return-assign
+const setButton = (buttonText) =>
+  (document.createElement('body').innerHTML = `<button>${buttonText}</button>`);
+// console.log(setButton('Magic Button'));
+
+// const setButton2 = (buttonText) => {
+//   document.querySelector('body').textContent = `<button>${buttonText}</button>`;
+// };
+
+// setButton2('javascript');
