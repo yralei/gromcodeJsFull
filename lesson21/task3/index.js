41e6065eaa3cb7d7995323aec49fcc74370a4368
Iurii <yralei@gmail.com>
2021-09-05 10:55:34 +0300

    lesson21task3

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,11 +1,11 @@
-const getItemsList = () => {
+export const getItemsList = () => {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 };
 // console.log(getItemsList());
 
-const getItemsArray = () => {
+export const getItemsArray = () => {
   const elementsArray = Array.from(document.querySelectorAll('.tool'));
   console.dir(elementsArray);
   return elementsArray;
