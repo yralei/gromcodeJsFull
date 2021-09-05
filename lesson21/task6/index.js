ded2ca1d371d5b01029d02eaac191144cb3e7e89
Iurii <yralei@gmail.com>
2021-09-05 13:52:09 +0300

    lesson21task6

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-return-assign
-export const setButton = (buttonText) =>
-  (document.createElement('body').innerHTML = `<button>${buttonText}</button>`);
+export const setButton = (buttonText) => {
+  document.querySelector('body').innerHTML = `<button>${buttonText}</button>`;
+};
 // console.log(setButton('Magic Button'));
 
 // const setButton2 = (buttonText) => {
 //   document.querySelector('body').textContent = `<button>${buttonText}</button>`;
 // };
 
-// setButton2('javascript');
+// setButton2('Magic');
