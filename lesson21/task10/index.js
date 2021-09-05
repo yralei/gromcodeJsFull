3e6b606db243badf4334b89db8ac6b0612ce648d
Iurii <yralei@gmail.com>
2021-09-05 16:33:10 +0300

    lesson21task10

---
+++ lesson21/task10/index.js
@@ -0,0 +1,8 @@
+export const finishForm = () => {
+  const loginForm = document.createElement('input');
+  loginForm.setAttribute('name', 'login');
+  loginForm.setAttribute('type', 'text');
+  document.querySelector('.login-form').prepend(loginForm);
+  document.querySelector('[name="password"]').setAttribute('type', 'password');
+};
+console.log(finishForm());
