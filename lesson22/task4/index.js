38474f59aea9522372b87e191422203087b6a226
Iurii <yralei@gmail.com>
2021-09-06 21:53:18 +0300

    l22t4

--- lesson22/task4/index.js
+++ lesson22/task4/index.js
@@ -1,4 +1,6 @@
 const checkboxElem = document.querySelector('.task-status');
-const checkboxElemFunc = () =>
+const checkboxElemFunc = () => {
+  console.log(checkboxElem.checked);
   checkboxElem.checked ? console.log('true') : console.log('false');
+};
 checkboxElem.addEventListener('change', checkboxElemFunc);
