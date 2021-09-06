06af5c4a34169766094a733ff5d755f4647c02b9
Iurii <yralei@gmail.com>
2021-09-06 22:08:11 +0300

    l22t5

--- lesson22/task5/index.js
+++ lesson22/task5/index.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line no-return-assign
-export const setTitle = (text) =>
-  (document.querySelector('.title').textContent = text);
-
-// console.log(setTitle('World is beatiful'));
+const searchBtn = document.querySelector('.search__btn');
+const searchInput = document.querySelector('.search__input');
+const searchBtnFunc = () => {
+  console.log(searchInput.value);
+};
+searchBtn.addEventListener('click', searchBtnFunc);
