a8cf342ff1267c0ea844e8ec42afbf85df6e3406
Iurii <yralei@gmail.com>
2021-09-06 19:17:26 +0300

    l22t2

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -1,13 +1,5 @@
-export const getTitleElement = () => {
-  const titleElem = document.querySelector('.title');
-  console.dir(titleElem);
-  return titleElem;
+const btn = document.querySelector('.create-btn');
+const alertFunc = () => {
+  alert('done');
 };
-// console.log(getTitleElement());
-
-export const getInputElement = () => {
-  const inputElem = document.querySelector('input[type=text]');
-  console.dir(inputElem);
-  return inputElem;
-};
-// console.log(getInputElement());
+btn.addEventListener('click', alertFunc);
