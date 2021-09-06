744ce126394e2f3c185aa422ef3e7b6f06aac5e5
Iurii <yralei@gmail.com>
2021-09-06 21:46:38 +0300

    l22t4

--- lesson22/task4/index.js
+++ lesson22/task4/index.js
@@ -1,11 +1,4 @@
-export const getTitle = () => document.querySelector('.title').textContent;
-// console.log(getTitle());
-
-export const getDescription = () => document.querySelector('.about').innerText;
-// console.log(getDescription());
-
-export const getPlans = () => document.querySelector('.plans').innerHTML;
-// console.log(getPlans());
-
-export const getGoal = () => document.querySelector('.goal').outerHTML;
-// console.log(getGoal());
+const checkboxElem = document.querySelector('.task-status');
+const checkboxElemFunc = () =>
+  checkboxElem.checked ? console.log('true') : console.log('false');
+checkboxElem.addEventListener('change', checkboxElemFunc);
