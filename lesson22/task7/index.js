ba14e383c7f2d0e6cb52945d77138a662ee07495
Iurii <yralei@gmail.com>
2021-09-07 13:31:35 +0300

    l22t7

--- lesson22/task7/index.js
+++ lesson22/task7/index.js
@@ -1,4 +1,9 @@
-// eslint-disable-next-line no-return-assign
-export const clearList = () =>
-  (document.querySelector('.categories').innerHTML = '');
-// clearList();
+const btn = document.querySelectorAll('.btn');
+const btnArray = Array.from(btn);
+console.log(btnArray);
+const handleClick = (event) => {
+  const text = event.target.textContent;
+  console.log(text);
+};
+btnArray[0].addEventListener('click', handleClick);
+btnArray[1].addEventListener('click', handleClick);
