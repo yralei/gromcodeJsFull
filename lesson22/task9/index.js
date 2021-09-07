7d9e655dd17eafe731d7b88fb53dcdcf60edf3d2
Iurii <yralei@gmail.com>
2021-09-07 14:06:17 +0300

    l22t9

--- lesson22/task9/index.js
+++ lesson22/task9/index.js
@@ -1,5 +1,6 @@
 const taskStatus = document.querySelector('.task-status');
 const taskStatusFunc = (event) => {
+  console.log(event.target.checked);
   const status = event.target.checked;
   console.log(status);
 };
