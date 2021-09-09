50a64ce55bcdf431b7ed15d72d456a498546d51c
Iurii <yralei@gmail.com>
2021-09-09 21:15:09 +0300

    l24t1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,8 +1,8 @@
-const dayOfWeekArr = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
-function getdayOfWeek(date, days) {
+export const dayOfWeek = (date, days) => {
+  const dayOfWeekArr = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
   const day = new Date(date).getDate();
   const dayInFuture = new Date(date).setDate(day + days);
   return dayOfWeekArr[new Date(dayInFuture).getDay()];
-}
-const result = getdayOfWeek(new Date(2019, 0, 1), 14);
+};
+const result = dayOfWeek(new Date(2019, 0, 1), 14);
 console.log(result);
