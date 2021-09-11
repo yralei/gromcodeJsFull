e650f0a6a9850b2046704a9a3642fe72d9049fa8
Iurii <yralei@gmail.com>
2021-09-11 15:28:29 +0300

    l27t1

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,14 +1,17 @@
-// localStorage.clear();
-// localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-// localStorage.setItem('name', JSON.stringify('Nick'));
-// localStorage.setItem('age', JSON.stringify(17));
+localStorage.clear();
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+localStorage.setItem('name', JSON.stringify('Nick'));
+localStorage.setItem('age', JSON.stringify(17));
 
-export const getLocalStorageData = () =>
-  Object.entries(localStorage).reduce(
-    (acc, [key, value]) => ({
-      ...acc,
-      [key]: JSON.parse(value),
-    }),
-    {}
-  );
-// console.log(getLocalStorageData());
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return { ...acc, [key]: newValue };
+  }, {});
+};
+console.log(getLocalStorageData());
