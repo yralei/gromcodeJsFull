1b71f1be9927ce69c8d79698ad9257c684c0f19a
Iurii <yralei@gmail.com>
2021-09-11 15:11:07 +0300

    l27t1

---
+++ lesson27/task1/index.js
@@ -0,0 +1,14 @@
+localStorage.clear();
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+localStorage.setItem('name', JSON.stringify('Nick'));
+localStorage.setItem('age', JSON.stringify(17));
+
+const getLocalStorageData = () =>
+  Object.entries(localStorage).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: JSON.parse(value),
+    }),
+    {}
+  );
+console.log(getLocalStorageData());
