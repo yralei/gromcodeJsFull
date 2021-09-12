c109e363863854a4c1a855a19169afbdfc35a27a
Iurii <yralei@gmail.com>
2021-09-12 17:05:21 +0300

    l28t2

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -1,28 +1,29 @@
-console.log('17 to string is ' + String(17));
-console.log('-17.17 to string is ' + String(-17.17));
-console.log('false to string is ' + String(false));
-console.log('null to string is ' + String(null));
-console.log('undefined to string is ' + String(undefined));
-console.log('0 to string is ' + String(0));
+// const favorites = ['id-6', 'id-17'];
 
-/* cast to number */
-console.log("'17' to number is " + Number('17'));
-console.log('true to number is ' + Number(true));
-console.log('false to number is ' + Number(false));
-console.log('null to number is ' + Number(null));
-console.log('undefined to number is ' + Number(undefined));
-console.log("'   20   ' to number is " + Number('   20   '));
-console.log("'      ' to number is " + Number('      '));
-console.log("'   30d   ' to number is " + Number('   30d   '));
+// const tree = {
+//   id: 'id-1',
+//   name: 'Products',
+//   nodes: [
+//     {
+//       id: 'id-2',
+//       name: 'Food',
+//       nodes: [{ id: 'id-6', name: 'Drinks', nodes: [] }],
+//     },
+//     {
+//       id: 'id-17',
+//       name: 'Vehicles',
+//       nodes: [],
+//     },
+//   ],
+// };
 
-/* cast to boolean */
-console.log('null to boolean is ' + Boolean(null));
-console.log('undefined to boolean is ' + Boolean(undefined));
-console.log('0 to boolean is ' + Boolean(0));
-console.log('-0 to boolean is ' + Boolean(-0));
-console.log('NaN to boolean is ' + Boolean(NaN));
-console.log("'' to boolean is " + Boolean(''));
-console.log("' ' to boolean is " + Boolean(' '));
-console.log('17 to boolean is ' + Boolean(17));
-console.log("'Hello' to boolean is " + Boolean('Hello'));
-console.log("'Hello' to boolean is" + Boolean('Hello'));
+const markFavorites = (tree, favorites) => {
+  const isFavorite = favorites.includes(tree.id);
+  return {
+    ...tree,
+    isFavorite,
+    nodes: tree.nodes.map((childNode) => markFavorites(childNode, favorites)),
+  };
+};
+// const result = markFavorites(tree, favorites);
+// console.log(result);
