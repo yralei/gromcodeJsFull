d42d3e85886afd2d0bc099826f707998ee7ff8c4
Iurii <yralei@gmail.com>
2021-09-12 17:07:46 +0300

    l28t2

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -17,7 +17,7 @@
 //   ],
 // };
 
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
   const isFavorite = favorites.includes(tree.id);
   return {
     ...tree,
