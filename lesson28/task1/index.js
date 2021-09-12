ebb12e29fe91d0a8ad546247ff7bc3f0caee0509
Iurii <yralei@gmail.com>
2021-09-12 15:56:56 +0300

    l28t1

---
+++ lesson28/task1/index.js
@@ -0,0 +1,27 @@
+const calc = (initValue) => {
+  let result = initValue;
+  const calculator = {
+    add(number) {
+      result += number;
+      return calculator;
+    },
+    subtract(number) {
+      result -= number;
+      return this;
+    },
+    mult(number) {
+      result *= number;
+      return this;
+    },
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+  return calculator;
+};
+const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
+console.log(result);
