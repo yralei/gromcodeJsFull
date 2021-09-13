2d035834293f8f465df74e9d3cac5999b4518640
Iurii <yralei@gmail.com>
2021-09-13 19:56:59 +0300

    l28t3

--- lesson28/task3/index.js
+++ lesson28/task3/index.js
@@ -1,6 +1,6 @@
-const mult = (a) => (b) => a * b;
-const twice = mult(2);
-const triple = mult(3);
+export const mult = (a) => (b) => a * b;
+export const twice = mult(2);
+export const triple = mult(3);
 
 // console.log(twice(10));
 // console.log(div(10)(2));
