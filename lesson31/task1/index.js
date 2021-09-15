227cfa1b5b6066921f5f450f9383ef9365a03e30
Iurii <yralei@gmail.com>
2021-09-15 20:44:05 +0300

    l31t1

---
+++ lesson31/task1/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// export
+const requestUserData = (userId) => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          email: `${userId}@example.com`,
+          userId,
+        });
+      }, 1000);
+    }
+  });
+  return request;
+};
+// requestUserData('broken')
+//   .catch((error) => {
+//     console.log(error);
+//     throw new Error('Error');
+//   })
+//   .then((data) => console.log(data))
+//   .finally(() => console.log('finally'));
+
+// requestUserData('userId777')
+//   .catch((error) => {
+//     console.log(error);
+//     throw new Error('Error');
+//   })
+//   .then((data) => console.log(data))
+//   .finally(() => console.log('finally'));
