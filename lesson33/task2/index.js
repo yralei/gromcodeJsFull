433efc5237b1a2c7fcc49c867a7c9ce8434b2c8e
Iurii <yralei@gmail.com>
2021-09-19 11:40:08 +0300

    L33T2

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,15 +1,11 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-function getTasksList() {
-  return fetch(baseUrl)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+export function getTasksList() {
+  return fetch(baseUrl).then((response) => response.json());
 }
 
-function getTaskById(taskId) {
-  return fetch(`${baseUrl}/${taskId}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+export function getTaskById(taskId) {
+  return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
 }
 
 // // examples
