5b3432304c4ceea7fddddc9a55586b017a7d45e2
Iurii <yralei@gmail.com>
2021-09-21 18:04:36 +0300

    l34t3

---
+++ lesson34/task3/index.js
@@ -0,0 +1,41 @@
+const baseUrl = 'https://61486eaf035b3600175b9e2d.mockapi.io/v1/users';
+const errorTextElem = document.querySelector('.error-text');
+const submitBtnElem = document.querySelector('.submit-button');
+const formElem = document.querySelector('.login-form');
+
+const validation = () => {
+  const isValide = formElem.reportValidity();
+  submitBtnElem.disabled = !isValide;
+  return submitBtnElem.disabled;
+};
+// eslint-disable-next-line arrow-body-style
+const addNewUser = (user) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(user),
+  });
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formData = [...new FormData()].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
+  addNewUser(formData)
+    .then((response) => response.json())
+    .then((user) => {
+      alert(JSON.stringify(user));
+      formElem.reset();
+      submitBtnElem.disabled = true;
+    })
+    .catch(() => {
+      errorTextElem.textContent = 'Failed to create user';
+      submitBtnElem.disabled = true;
+    });
+}
+formElem.addEventListener('submit', onFormSubmit);
+formElem.addEventListener('input', validation);
