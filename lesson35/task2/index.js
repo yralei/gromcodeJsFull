efa6172a7aa1aeefd5062d01d63fbf1aadedded9
Iurii <yralei@gmail.com>
2021-09-22 09:45:34 +0300

    L35T2

--- lesson35/task2/index.js
+++ lesson35/task2/index.js
@@ -1,32 +1,23 @@
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve({ name: 'Tom', age: 17 });
-  }, 1000);
-});
+const successRequest = Promise.resolve({ name: 'Tom' });
 
-/*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
- */
+successRequest
+  .then(function onSuccess1(data) {
+    throw new Error('Error with data');
+  })
+  .catch(function onError1(error) {
+    console.error('onError1', error.message);
+  });
 
-console.log(userDataPromise);
+const failRequest = Promise.reject(new Error('Something went wrong'));
 
-/*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData?
- */
-userDataPromise.then(function onSuccess(userData) {
-  console.log(userData);
-});
-
-/*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
- */
-
-userDataPromise.then(function onSuccess(data) {
-  console.log(`My name is ${data.name}. I am ${data.age} years old`);
-});
+failRequest
+  .catch(function onError2(error) {
+    console.error('onError2', error.message);
+    throw new Error('Server error');
+  })
+  .then(function onSuccess2(data) {
+    console.log('onSuccess2', data);
+  })
+  .catch(function onError3(error) {
+    console.error('onError3', error.message);
+  });
